5b7527708c1e4874bcb1116880197d45e96b6232
beth92 <bethtownsend92@gmail.com>
2018-02-03 15:54:53 -0500

    Add css to chat page

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,10 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 });
+
 socket.on('disconnect', function () {
   console.log('disconnected from server');
 });
-
 
 socket.on('newMessage', function (msg){
   var li = $('<li></li>');
@@ -27,12 +27,15 @@ $('#message-form').on('submit', function (e) {
   // prevent default behavior of submit event (refresh)
   e.preventDefault();
 
+  var messageTextbox = $('[name=message]');
+
   socket.emit('createMessage', {
     from: 'user',
-    text: $('[name=message]').val(),
+    text: messageTextbox.val(),
     createdAt: new Date().getTime()
-  }, function (s) {
-    console.log(s);
+  }, function () {
+    // clear text input when message sent
+    messageTextbox.val('');
   });
 });
 
@@ -40,13 +43,16 @@ $('#send-location').on('click', function(){
   if(!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
   }
-
+  var locationBtn = $(this);
+  locationBtn.attr('disabled', 'disabled').text('Sending location...');
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationBtn.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   },function() {
-  alert('Unable to fetch location.');
-});
+    locationBtn.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location.');
+  });
 });
